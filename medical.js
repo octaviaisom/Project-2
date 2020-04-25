bf89c190b8a661bee3f2a001d84823c485b7a9a0
SadiaS-96 <59122920+SadiaS-96@users.noreply.github.com>
2020-04-25 10:47:16 -0400

    js files

--- medical.js
+++ medical.js
@@ -1,6 +1,6 @@
 function drawChart() {
 
-	d3.csv("FTSE.csv").then(function(prices) {
+	d3.csv("stock_data.csv").then(function(prices) {
 		
 		const months = {0 : 'Jan', 1 : 'Feb', 2 : 'Mar', 3 : 'Apr', 4 : 'May', 5 : 'Jun', 6 : 'Jul', 7 : 'Aug', 8 : 'Sep', 9 : 'Oct', 10 : 'Nov', 11 : 'Dec'}
 
